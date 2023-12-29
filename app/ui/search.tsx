d19bf5bc00bfd64ef241477e05464bf8cd3e2037
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-12-28 19:01:06 -0700

    Implement pagination with URL search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,9 +11,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
   
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`)
-
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
