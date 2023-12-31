4d4d8789e0b504af7b932521b30c1034a3e0fdef
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-12-30 18:57:47 -0700

    Implement login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | advocate.ai',
+    default: 'advocate.ai',
+  },
+  description: 'A personalized health advocate that helps you prepare for, record, and review your medical appointments.',
+  metadataBase: new URL('https://https://advocateai.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '../auth';
  
 const FormSchema = z.object({
     id: z.string(),
@@ -114,3 +116,22 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
     };
     message?: string | null;
   };
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
