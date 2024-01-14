8b754190f0f52e643ea2d011585be18a706c744f
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-01-14 15:18:32 -0700

    Implement working audio upload component

--- app/dashboard/appointments/create/AudioUpload.tsx
+++ app/dashboard/appointments/create/AudioUpload.tsx
@@ -1,43 +1,109 @@
 'use client'
 import React, { useEffect, useState, useRef } from 'react'
+import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/app/database.types'
+import * as tus from 'tus-js-client'
 
-
-
-export default function AudioUpload(){
-
+export default function AudioUpload({ session }: { session: Session | null }) {
     const inputFileRef = useRef<HTMLInputElement>(null);
     const [isUploading, setIsUploading] = useState(false);
-    
-    return (
-    <fieldset>
-    <legend className="mb-2 block text-sm font-medium">
-    Appointment Recording
-    </legend>
-    <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-    <div className="flex gap-4">
-        <div className="flex items-center">
-        
-        <input
-            id="audio_path"
-            name="audio_path"
-            ref={inputFileRef}
-            aria-describedby='audio-error'
-            type="file"
-            accept="audio/mpeg, audio/mp3"
-            className="cursor-pointer text-sm border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-        />
-        <button type="button" className="border p-2 mx-4" onClick={handleAudioUpload}>
-            Upload Audio
-        </button>
-        {isUploading && 
-        <div>audio uploading</div>
+    const supabase = createClientComponentClient<Database>();
+    const user = session?.user
+    const userId = user?.id
+
+    async function handleAudioUpload() {
+        const fileInput = inputFileRef.current;
+
+        if (fileInput && fileInput.files && fileInput.files.length > 0) {
+            const file = fileInput.files[0];
+
+            try {
+                setIsUploading(true);
+
+                await uploadFile('apptrecordings', `${userId}/testuploadfilename`, file);
+
+                setIsUploading(false);
+            } catch (error) {
+                console.error('Error uploading file:', error);
+                setIsUploading(false);
+            }
         }
-        
-        </div>
-        
-    </div>
-    </div>
-    
-</fieldset>
+    }
+
+    async function uploadFile(bucketName: string, fileName: string, file: File) {
+
+        return new Promise((resolve, reject) => {
+            const upload = new tus.Upload(file, {
+                endpoint: `https://tmmnudhjtavobvreaink.supabase.co/storage/v1/upload/resumable`,
+                retryDelays: [0, 3000, 5000, 10000, 20000],
+                headers: {
+                    authorization: `Bearer ${session.access_token}`,
+                    'x-upsert': 'true',
+                },
+                uploadDataDuringCreation: true,
+                removeFingerprintOnSuccess: true,
+                metadata: {
+                    bucketName,
+                    objectName: fileName,
+                    contentType: file.type,
+                    cacheControl: 3600,
+                },
+                chunkSize: 6 * 1024 * 1024,
+                onError: function (error) {
+                    console.log('Failed because: ' + error);
+                    reject(error);
+                },
+                onProgress: function (bytesUploaded, bytesTotal) {
+                    var percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+                    console.log(bytesUploaded, bytesTotal, percentage + '%');
+                },
+                onSuccess: function () {
+                    console.log('Download %s from %s', upload.file.name, upload.url);
+                    resolve();
+                },
+            });
+
+            // Check if there are any previous uploads to continue.
+            upload.findPreviousUploads().then(function (previousUploads) {
+                if (previousUploads.length) {
+                    upload.resumeFromPreviousUpload(previousUploads[0]);
+                }
+
+                upload.start();
+            });
+        });
+    }
+
+    return (
+        <fieldset>
+            <legend className="mb-2 block text-sm font-medium">
+                Appointment Recording
+            </legend>
+            <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+                <div className="flex gap-4">
+                    <div className="flex items-center">
+
+                        <input
+                            id="audio_path"
+                            name="audio_path"
+                            ref={inputFileRef}
+                            aria-describedby='audio-error'
+                            type="file"
+                            accept="audio/mpeg, audio/mp3"
+                            className="cursor-pointer text-sm border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                        />
+                        <button type="button" className="border p-2 mx-4" onClick={handleAudioUpload}>
+                            Upload Audio
+                        </button>
+                        {isUploading &&
+                            <div>audio uploading</div>
+                        }
+
+                    </div>
+
+                </div>
+            </div>
+
+        </fieldset>
     )
 }
--- app/dashboard/appointments/create/create-form.tsx
+++ app/dashboard/appointments/create/create-form.tsx
@@ -25,6 +25,152 @@ export default function CreateAppointment({ session }: { session: Session | null
   const [recording_url, setRecordingUrl] = useState<string | null>(null)
   const user = session?.user
 
+
+  function createAppointment(){
+    return
+  }
+
+
+  return (
+    <form action={createAppointment}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        
+        {/* Appointment Title */}
+        <div className="mb-4">
+          <label htmlFor="title" className="mb-2 block text-sm font-medium">
+            Appointment Title
+          </label>
+          <div className="relative">
+            <input
+              id="title"
+              name="title"
+              type='text'
+              placeholder='Appointment name'
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              value={title || ''}
+              onChange={(e) => setTitle(e.target.value)}
+            >
+              
+            </input>
+            <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+        </div>
+        
+        {/* Description */}
+        <div className="mb-4">
+          <label htmlFor="description" className="mb-2 block text-sm font-medium">
+            Appointment Description
+          </label>
+          <div className="relative">
+            <input
+              id="description"
+              name="description"
+              required
+              placeholder='Brief description of appointment'
+              type='text'
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              
+              aria-describedby='description-error'
+              value={description || ''}
+              onChange={(e) => setDescription(e.target.value)}
+            >
+              
+            </input>
+            <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          
+        </div>
+
+        {/* Provider */}
+        <div className="mb-4">
+          <label htmlFor="provider" className="mb-2 block text-sm font-medium">
+            Provider
+          </label>
+          <div className="relative">
+            <input
+              id="provider"
+              name="provider"
+              placeholder='Doctor or name of provider(s) seen during visit'
+              type='text'
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              value={provider || ''}
+              onChange={(e) => setProvider(e.target.value)}
+              aria-describedby='provider-error'
+            >
+              
+            </input>
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          
+        </div>
+
+        {/* Clinic */}
+        <div className="mb-4">
+          <label htmlFor="clinic" className="mb-2 block text-sm font-medium">
+            Clinic
+          </label>
+          <div className="relative">
+            <input
+              id="clinic"
+              name="clinic"
+              placeholder='Clinic, facility name'
+              type='text'
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              value={clinic || ''}
+              onChange={(e) => setClinic(e.target.value)}
+              aria-describedby='clinic-error'
+            >
+              
+            </input>
+            <BuildingOffice2Icon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          
+        </div>
+
+        {/* Date */}
+        <div className="mb-4">
+          <label htmlFor="appointment_date" className="mb-2 block text-sm font-medium">
+            Appointment Date
+          </label>
+          <div className="relative">
+            <input
+              id="appointment_date"
+              name="appointment_date"
+              type="date"
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            >
+              
+            </input>
+            <CalendarDaysIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          
+        </div>
+
+        
+        
+        {/* Appointment Recording */}
+        <AudioUpload session={session} />
+        
+
+
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/appointments"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Add Appointment</Button>
+      </div>
+    </form>
+  )
+}
+
+  
+  
+
+
 //   const getProfile = useCallback(async () => {
 //     try {
 //       setLoading(true)
@@ -85,169 +231,3 @@ export default function CreateAppointment({ session }: { session: Session | null
 //       setLoading(false)
 //     }
 //   }
-
-  function createAppointment(){
-    return
-  }
-
-
-  return (
-    <form action={createAppointment}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        
-        {/* Appointment Title */}
-        <div className="mb-4">
-          <label htmlFor="title" className="mb-2 block text-sm font-medium">
-            Appointment Title
-          </label>
-          <div className="relative">
-            <input
-              id="title"
-              name="title"
-              type='text'
-              placeholder='Appointment name'
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              value={title || ''}
-              onChange={(e) => setTitle(e.target.value)}
-            >
-              
-            </input>
-            <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-        </div>
-        
-        {/* Description */}
-        <div className="mb-4">
-          <label htmlFor="description" className="mb-2 block text-sm font-medium">
-            Appointment Description
-          </label>
-          <div className="relative">
-            <input
-              id="description"
-              name="description"
-              required
-              placeholder='Brief description of appointment'
-              type='text'
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby='description-error'
-              value={description || ''}
-              onChange={(e) => setDescription(e.target.value)}
-            >
-              
-            </input>
-            <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          
-        </div>
-
-        {/* Provider */}
-        <div className="mb-4">
-          <label htmlFor="provider" className="mb-2 block text-sm font-medium">
-            Provider
-          </label>
-          <div className="relative">
-            <input
-              id="provider"
-              name="provider"
-              placeholder='Doctor or name of provider(s) seen during visit'
-              type='text'
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby='provider-error'
-            >
-              
-            </input>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          
-        </div>
-
-        {/* Clinic */}
-        <div className="mb-4">
-          <label htmlFor="clinic" className="mb-2 block text-sm font-medium">
-            Clinic
-          </label>
-          <div className="relative">
-            <input
-              id="clinic"
-              name="clinic"
-              placeholder='Clinic, facility name'
-              type='text'
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby='clinic-error'
-            >
-              
-            </input>
-            <BuildingOffice2Icon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          
-        </div>
-
-        {/* Date */}
-        <div className="mb-4">
-          <label htmlFor="appointment_date" className="mb-2 block text-sm font-medium">
-            Appointment Date
-          </label>
-          <div className="relative">
-            <input
-              id="appointment_date"
-              name="appointment_date"
-              type="date"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby='date-error'
-            >
-              
-            </input>
-            <CalendarDaysIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          
-        </div>
-
-        {/* Amount Paid */}
-        <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Amount Paid
-          </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id="amount"
-                name="amount"
-                type="number"
-                step="0.01"
-                min="0"
-                placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                aria-describedby='amount-error'
-              />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-          
-        </div>
-        
-        {/* Appointment Recording */}
-        <AudioUpload />
-        
-
-
-      </div>
-      <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/appointments"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
-        <Button type="submit">Add Appointment</Button>
-      </div>
-    </form>
-  )
-}
-
-  
-  
