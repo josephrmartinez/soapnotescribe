5827890dd565985e28ee857e1b75b3535a9e6108
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-02-16 07:40:02 -0700

    Initialize create agent form

--- app/dashboard/agents/page.tsx
+++ app/dashboard/agents/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 import { GeistSans } from 'geist/font/sans';
 import { PencilIcon, PlusIcon, TrashIcon, ArrowRightIcon, DocumentPlusIcon } from '@heroicons/react/24/outline';
 import AgentCard from '@/app/ui/agents/agent-card';
@@ -18,12 +19,14 @@ export default function Page() {
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <div className='text-gray-700'>Create and manage AI-powered agents to send reminders or perform research.</div>
-        <button
+        <Link
+      href="/dashboard/agents/create"
       className="flex h-10 items-center rounded-lg bg-teal-600 px-4 text-sm font-medium text-white transition-colors hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      
     >
       <span className="hidden md:block">Create Agent</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
-    </button>
+    </Link>
       </div>
       <AgentCard underlineText="Email me" normalText="a weekly newsletter with the latest research on embryo testing."/>
       <AgentCard underlineText="Text me" normalText="a daily reminder at 1pm to take 1 tsp omega-3 after lunch."/>
