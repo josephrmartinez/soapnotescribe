a17989b6b50bea565a9f28017e02e30da3e005cd
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-03-05 18:12:53 -0700

    Update AIChat component

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { Message, OpenAIStream, StreamingTextResponse } from 'ai';
 
 
 const openai = new OpenAI({
@@ -9,19 +9,34 @@ const openai = new OpenAI({
 
 export const runtime = 'edge';
 
-export async function POST(req: Request, res: NextResponse) {
-  const body = await req.json();
+export async function POST(req: Request) {
+  try {
+  const { messages } = await req.json();
   // console.log("req body:", body);
+
+
+  const prompt = [
+    {
+      role: "system",
+      content: `You are a medical advocate....`
+    },
+  ];
 
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-0125",
-    messages: body.messages,
+    messages: [
+      ...prompt,
+      // filter messages to only send user's messages to AI API
+      ...messages.filter((message: Message) => message.role === "user")
+    ],
     stream: true
   });
   
   // UPDATE UI TO ACCOMODATE OPENAISTREAM
   const stream = OpenAIStream(response)
   return new StreamingTextResponse(stream)
+} catch (e) {
+  throw e
+}
 
-  // return NextResponse.json({ output: response }, { status: 200 });
 }
