f8397cb69e13843514a410cb45aff6184d84dc7f
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-04-11 10:20:53 -0700

    Create dashboard redirect route

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,22 +1,5 @@
-import { redirect } from "next/navigation";
-import { createClient } from "@/utils/supabase/server";
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const supabase = createClient()
-
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    console.error(error)
-    redirect('/error')
-  }
-
-
-  return (
-    <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>
-        Dashboard for {data.user.email}
-      </h1>
-      
-    </main>
-  );
+  redirect('/dashboard/notes');
 }
