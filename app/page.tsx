8af0324db3dfc3c851392eb5c1cc3683237124e4
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-04-15 18:30:08 -0700

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
 import { PricingCard } from './ui/PricingCard';
 import { UseStep } from './ui/UseStep';
 import Link from 'next/link';
+import Image from 'next/image';
 import Header from './ui/Header';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
@@ -24,15 +25,12 @@ export default async function Page() {
     <main className="flex h-full flex-col p-2">
       <Header />
 
-      <div className="mx-auto max-w-screen-lg">
-        <p className="mx-auto mb-8 mt-20 text-center text-5xl font-bold text-gray-900 ">
-          Automate your SOAP notes
+      <div className="mx-auto">
+        <p className="mx-auto mb-8 mt-20 text-center text-5xl font-black text-gray-700 ">
+          Automate your clinical charting.
         </p>
-
-        <p className="mx-auto  max-w-prose py-4 text-center text-xl text-gray-600">
-          Upload appointment audio recordings or post-appointment voice memos
-          and soapscribe does the rest - analyzing, structuring, and saving
-          notes securely in a HIPAA-compliant database.
+        <p className="mx-auto text-center text-3xl font-semibold text-gray-600">
+          Go from audio memo to structured SOAP note in seconds.
         </p>
 
         <div className="mx-auto my-8 flex flex-row items-center justify-center align-middle">
@@ -49,32 +47,44 @@ export default async function Page() {
             see pricing
           </Link>
         </div>
-
         <p className="mx-auto mb-8 mt-20 text-center text-3xl font-bold text-gray-600 dark:text-gray-400">
-          Easy to use:
+          Easy to use. No setup required.
         </p>
-        <div className="mx-auto my-20 grid max-w-screen-xl grid-cols-2 gap-4">
+        <div className="flex w-full flex-col items-center bg-gray-200 px-8 py-16">
+          <Image
+            alt="soapscribe demo"
+            width={1000}
+            height={1000}
+            src="/soapscribedemo.gif"
+            unoptimized={true}
+            className="rounded-lg shadow-lg"
+          ></Image>
+        </div>
+        <div className="mx-auto my-20 grid max-w-screen-xl grid-cols-3 gap-1">
           <UseStep
             icon={<MicrophoneIcon />}
             step="1. Upload audio"
-            subtext="Upload post-appointment clinical note voice memos or a recording of the entire appointment. Soapscribe will intelligently extract the relevant information."
+            subtext="Upload clinical audio memos or a recording of the entire appointment. Soapscribe will intelligently extract the relevant information."
           />
-          <div className="rounded-lg border"></div>
+
           <UseStep
             icon={<PencilSquareIcon />}
-            step="2. Edit draft"
+            step="2. Review draft"
             subtext="A structured SOAP note is available in seconds for easy review and approval. Make quick edits instead of starting each note from scratch."
           />
-          <div className="rounded-lg border"></div>
+
           <UseStep
             icon={<ArchiveBoxIcon />}
             step="3. Approve and save"
             subtext="Your notes are saved in a HIPPA-compliant database that is easy to search and review."
           />
-          <div className="rounded-lg border"></div>
         </div>
 
-        <p className="mx-auto mb-8 mt-20 text-center text-3xl font-bold text-gray-600 ">
+        <div className="rounded-lg border"></div>
+        <div className="rounded-lg border"></div>
+        <div className="rounded-lg border"></div>
+
+        {/* <p className="mx-auto mb-8 mt-20 text-center text-3xl font-bold text-gray-600 ">
           Simple pricing
         </p>
 
@@ -96,7 +106,7 @@ export default async function Page() {
             featureTwo="Unlimited recording length"
             featureThree="Create custom templates"
           />
-        </div>
+        </div> */}
       </div>
     </main>
   );
