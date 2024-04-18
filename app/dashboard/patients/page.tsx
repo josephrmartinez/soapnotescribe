9cda63f37dbf79eb971367d7c2ee0d8b30dbfa74
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-04-17 19:55:09 -0700

    Create patients route

--- app/dashboard/patients/page.tsx
+++ app/dashboard/patients/page.tsx
@@ -2,14 +2,15 @@ import { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 
 export const metadata: Metadata = {
-  title: 'Templates',
+  title: 'Patients',
 };
 
 export default function Page() {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${GeistSans.className} text-2xl`}>Templates</h1>
+        <h1 className={`${GeistSans.className} text-2xl`}>Patients</h1>
+        <button className="roounded-lg border">Add Patient</button>
       </div>
     </div>
   );
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import {
   UserGroupIcon,
@@ -7,26 +7,36 @@ import {
   ArchiveBoxIcon,
   Cog8ToothIcon,
   UserCircleIcon,
-  ChatBubbleLeftEllipsisIcon
-  
+  ChatBubbleLeftEllipsisIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'SOAP Notes', href: '/dashboard/notes', icon: ArchiveBoxIcon },
-  { name: 'Write New Note', href: '/dashboard/createnote', icon: PencilSquareIcon },
-  { name: 'Create Note from Audio', href: '/dashboard/audionote', icon: ChatBubbleLeftEllipsisIcon },
-  { name: 'Templates', href: '/dashboard/templates', icon: DocumentDuplicateIcon }
-  // {
-  //   name: 'Account',
-  //   href: '/dashboard/settings',
-  //   icon: Cog8ToothIcon,
-  // }
+  {
+    name: 'Write New Note',
+    href: '/dashboard/createnote',
+    icon: PencilSquareIcon,
+  },
+  {
+    name: 'Create Note from Audio',
+    href: '/dashboard/audionote',
+    icon: ChatBubbleLeftEllipsisIcon,
+  },
+  {
+    name: 'Templates',
+    href: '/dashboard/templates',
+    icon: DocumentDuplicateIcon,
+  },
+  {
+    name: 'Patients',
+    href: '/dashboard/patients',
+    icon: UserGroupIcon,
+  },
 ];
 
 export default function NavLinks() {
@@ -43,9 +53,10 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md border border-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-teal-600' : pathname === link.href
+                'bg-sky-100 text-teal-600': pathname === link.href,
               },
-            )}>
+            )}
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
