e284f4157fb6a58f092aefc352f24be188bf78d1
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-05-18 18:56:16 -0700

    Update client side with patient info

--- app/dashboard/newnote/create-form.tsx
+++ app/dashboard/newnote/create-form.tsx
@@ -39,14 +39,23 @@ interface PatientOption {
 
 export default function CreateAppointment() {
   const [loading, setLoading] = useState(true);
-  const [patientName, setPatientName] = useState<string | null>(null);
+  const [patientId, setPatientId] = useState<string>('');
+
+  const [dateOfBirth, setDateOfBirth] = useState<string>('');
+  const [phone, setPhone] = useState<string | null>('');
+  const [email, setEmail] = useState<string>('');
+  const [addressStreet, setAddressStreet] = useState<string | null>('');
+  const [addressUnit, setAddressUnit] = useState<string | null>('');
+  const [city, setCity] = useState<string | null>('');
+  const [state, setState] = useState<string | null>(null);
+  const [zipcode, setZipcode] = useState<string | null>('');
+  const [allergies, setAllergies] = useState<string>('');
+
+  const [profileNotes, setProfileNotes] = useState<string | null>(null);
+
   const [chiefComplaint, setChiefComplaint] = useState<string | null>(null);
   const [date, setDate] = useState<string>('');
   const [time, setTime] = useState<string>('');
-  const [patientId, setPatientId] = useState<string>('');
-
-  const [patientDateOfBirth, setPatientDateOfBirth] = useState<string>('');
-  const [allergies, setAllergies] = useState<string>('');
   const [consent, setConsent] = useState<string | null>(null);
   const [subjective, setSubjective] = useState<string | null>(null);
   const [objective, setObjective] = useState<string | null>(null);
@@ -93,6 +102,14 @@ export default function CreateAppointment() {
     if (newValue) {
       setPatientId(newValue.value.id);
       setAllergies(newValue.value.allergies);
+      setDateOfBirth(newValue.value.date_of_birth);
+      setAddressStreet(newValue.value.address_street);
+      setAddressUnit(newValue.value.address_unit);
+      setCity(newValue.value.city);
+      setState(newValue.value.state);
+      setZipcode(newValue.value.zipcode);
+      setEmail(newValue.value.email);
+      setPhone(newValue.value.phone);
     } else {
       console.log('No new patient value');
     }
@@ -130,15 +147,115 @@ export default function CreateAppointment() {
               </label>
               <SelectPatient onPatientSelect={handlePatientSelect} />
             </div>
-            <div className="mb-4 w-full text-center">
+            {/* <div className="mb-4 w-full text-center">
               <Link
                 className="text-sm italic text-teal-700 underline underline-offset-4"
                 href={'./patients/add'}
               >
                 add new patient
               </Link>
+            </div> */}
+          </div>
+
+          <div className="flex flex-row items-center">
+            {patientId === '' ? (
+              <div className="flex h-10 items-center rounded-lg bg-teal-600 px-4 text-sm font-medium text-white shadow transition-colors hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-700 active:bg-teal-600">
+                add new patient
+              </div>
+            ) : (
+              <div className="flex h-10 items-center rounded-lg bg-teal-600 px-4 text-sm font-medium text-white shadow transition-colors hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-700 active:bg-teal-600">
+                edit patient info
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className={`${patientId === '' ? 'hidden' : ''}`}>
+          <div className="grid grid-cols-2 gap-8">
+            {/* Patient Date of Birth */}
+            <div className="mb-4">
+              <label
+                htmlFor="appointment_date"
+                className="mb-2 block text-sm font-medium"
+              >
+                Date of Birth
+              </label>
+              <div className="relative">
+                <div>{dateOfBirth}</div>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="mb-2 block text-sm font-medium">
+                Phone Number
+              </label>
+              <div className="relative">{phone}</div>
             </div>
           </div>
+
+          <div className="grid grid-cols-2 gap-8">
+            <div className="mb-4">
+              <label htmlFor="email" className="mb-2 block text-sm font-medium">
+                Email Address
+              </label>
+              <div className="relative">{email}</div>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="complaint"
+                className="mb-2 block text-sm font-medium"
+              >
+                Street Address
+              </label>
+              <div className="relative">
+                <div>{addressStreet}</div>
+                {addressUnit && <div>{addressUnit}</div>}
+                <div>
+                  {city}, {state} {zipcode}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="profilenotes"
+              className="mb-2 block text-sm font-medium"
+            >
+              Patient Profile Notes
+            </label>
+            <div className="relative">
+              <textarea
+                id="profile_notes"
+                name="profile_notes"
+                className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 text-sm outline-2 placeholder:text-gray-500"
+                value={profileNotes || ''}
+                onChange={(e) => setProfileNotes(e.target.value)}
+              ></textarea>
+            </div>
+          </div>
+
+          {/* Allergies */}
+          <div className="mb-4">
+            <label
+              htmlFor="allergies"
+              className="mb-2 block text-sm font-medium"
+            >
+              Allergies
+            </label>
+            <div className="relative">
+              <input
+                id="allergies"
+                name="allergies"
+                required
+                type="text"
+                className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 text-sm outline-2 placeholder:text-gray-500"
+                value={allergies || ''}
+                onChange={(e) => setAllergies(e.target.value)}
+              ></input>
+            </div>
+          </div>
+
           {/* Telemedicine Consent */}
           <div className="mb-4">
             <label htmlFor="consent" className="mb-2 block text-sm font-medium">
@@ -158,10 +275,6 @@ export default function CreateAppointment() {
               <div className="text-sm">Patient consents to treatment.</div>
             </div>
           </div>
-        </div>
-
-        <div className="grid grid-cols-2 gap-8">
-          <div className="mb-4"></div>
         </div>
 
         <div className="grid grid-cols-2 gap-8">
@@ -299,7 +412,7 @@ export default function CreateAppointment() {
             Cancel
           </Link>
           <Button type="submit" active={submitOkay}>
-            Add Appointment
+            Add Note
           </Button>
         </div>
       </div>
