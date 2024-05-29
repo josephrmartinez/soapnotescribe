1305250872829cfecfd7876a89c518f4ef635799
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-05-29 14:57:52 -0400

    Update config to ignore type errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@
 // module.exports = nextConfig
 
 module.exports = {
+  eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
     experimental: {
       missingSuspenseWithCSRBailout: false,
     }
