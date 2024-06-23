aaf7530b81d49574691f085e90649622e23042e9
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-06-23 19:00:06 -0400

    Move fetchTemplates function

--- app/dashboard/templates/page.tsx
+++ app/dashboard/templates/page.tsx
@@ -1,18 +1,14 @@
-'use client';
-
 import { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { PlusLinkButton } from '@/app/ui/Buttons';
 import { fetchTemplates } from '@/app/lib/data';
 import TemplateTable from '@/app/ui/templates/TemplateTable';
 
-// export const metadata: Metadata = {
-//   title: 'Templates',
-// };
+export const metadata: Metadata = {
+  title: 'Templates',
+};
 
 export default async function Page() {
-  const userTemplates = (await fetchTemplates()) || [];
-
   return (
     <div className="w-full">
       <div className="mb-8 flex w-full">
@@ -20,7 +16,7 @@ export default async function Page() {
       </div>
       <PlusLinkButton href="./templates/add" text="Create Template" />
       <div className="mt-6">
-        <TemplateTable userTemplates={userTemplates} />
+        <TemplateTable />
       </div>
     </div>
   );
--- app/ui/templates/TemplateTable.tsx
+++ app/ui/templates/TemplateTable.tsx
@@ -1,20 +1,24 @@
 import Link from 'next/link';
 import { Template } from '@/app/lib/definitions';
+import { fetchTemplates } from '@/app/lib/data';
 
-export default function TemplateTable({
-  userTemplates,
-}: {
-  userTemplates: Template[];
-}) {
-  return userTemplates.map((template) => (
-    <Link
-      href={`/dashboard/templates/${template.id}`}
-      key={template.id}
-      className="flex h-10 cursor-pointer flex-row items-center font-medium transition-all hover:text-teal-600"
-    >
-      <div>{template.chief_complaint}</div>
-      <div className="mx-2">-</div>
-      <div>{template.soap_assessment}</div>
-    </Link>
-  ));
+export default async function TemplateTable() {
+  const userTemplates = await fetchTemplates();
+
+  return (
+    <div>
+      {userTemplates &&
+        userTemplates.map((template: Template) => (
+          <Link
+            href={`/dashboard/templates/${template.id}`}
+            key={template.id}
+            className="flex h-10 cursor-pointer flex-row items-center font-medium transition-all hover:text-teal-600"
+          >
+            <div>{template.chief_complaint}</div>
+            <div className="mx-2">-</div>
+            <div>{template.soap_assessment}</div>
+          </Link>
+        ))}
+    </div>
+  );
 }
