8e59fcbb08cdba36b61e13751856e2e936b9b9bf
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-07-01 15:54:27 -0400

    Create fademask tailwind utility class for templates

--- app/ui/templates/templatetable.tsx
+++ app/ui/templates/templatetable.tsx
@@ -7,17 +7,25 @@ export default async function TemplateTable() {
   console.log('templates:', userTemplates);
 
   return (
-    <div>
+    <div className="grid max-w-prose grid-cols-1 gap-4  sm:grid-cols-2">
       {userTemplates?.map((template: Template) => (
-        <Link
-          href={`/dashboard/templates/${template.id}`}
-          key={template.id}
-          className="flex h-10 cursor-pointer flex-row items-center font-medium transition-all hover:text-teal-600"
-        >
-          <div>{template.chief_complaint}</div>
-          <div className="mx-2">-</div>
-          <div>{template.soap_assessment}</div>
-        </Link>
+        <div className="rounded-md border">
+          <Link
+            href={`/dashboard/templates/${template.id}`}
+            key={template.id}
+            className="fademask group grid h-48 max-w-md cursor-pointer grid-rows-6 items-center  overflow-clip rounded-md   p-2 font-medium transition-all hover:shadow"
+          >
+            <div className="text-xl font-semibold text-gray-700 transition-colors group-hover:text-teal-600 ">
+              {template.chief_complaint}
+            </div>
+            <div className="text-md row-span-2 text-gray-600 transition-colors group-hover:text-teal-600">
+              {template.soap_assessment}
+            </div>
+            <div className=" row-span-3 max-w-md  text-sm text-gray-600 group-hover:text-gray-500">
+              {template.soap_plan}
+            </div>
+          </Link>
+        </div>
       ))}
     </div>
   );
