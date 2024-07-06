c6461a6e775144932f1ef259c3ee183e3d604fc7
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-07-05 20:52:27 -0400

    Remove AudioUploadRecord from test route

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
-import AudioUploadRecord from '@/app/components/AudioUploadRecord';
+// import AudioUploadRecord from '@/app/components/AudioUploadRecord';
 
 export const metadata: Metadata = {
   title: 'Settings',
@@ -11,7 +11,7 @@ export default function Page() {
     <div className="w-full">
       <div className="mb-8 flex w-full">
         <h1 className={`${GeistSans.className} text-2xl`}>Settings</h1>
-        <AudioUploadRecord patientId="123" />
+        {/* <AudioUploadRecord patientId="123" /> */}
       </div>
     </div>
   );
