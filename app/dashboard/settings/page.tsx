ccd3ebcd8e090ce459d12c8456004bda27e0cb7b
Joseph Martinez <joseph.r.martinez@gmail.com>
2024-07-05 20:33:48 -0400

    Recreate hidden settings page

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import AudioUploadRecord from '@/app/components/AudioUploadRecord';
+
+export const metadata: Metadata = {
+  title: 'Settings',
+};
+
+export default function Page() {
+  return (
+    <div className="w-full">
+      <div className="mb-8 flex w-full">
+        <h1 className={`${GeistSans.className} text-2xl`}>Settings</h1>
+        <AudioUploadRecord patientId="123" />
+      </div>
+    </div>
+  );
+}
